a2e5abad9a6adcfdbe8ce3ad97b1695357247d59
g185 <giuliano.martinelli97@gmail.com>
2019-06-20 15:38:15 +0200

    boh

--- src/main/java/it/uniroma3/siw/demospring/controller/SilphController.java
+++ src/main/java/it/uniroma3/siw/demospring/controller/SilphController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import it.uniroma3.siw.demospring.model.Album;
