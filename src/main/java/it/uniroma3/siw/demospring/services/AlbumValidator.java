28cc91ccaef8d8eb591671564431d2d93caeb012
Mattia <m.pocci@araneum.it>
2019-06-20 12:07:12 +0200

    validator del album quasi finito

--- src/main/java/it/uniroma3/siw/demospring/services/AlbumValidator.java
+++ src/main/java/it/uniroma3/siw/demospring/services/AlbumValidator.java
@@ -2,6 +2,7 @@ package it.uniroma3.siw.demospring.services;
 
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import it.uniroma3.siw.demospring.model.Album;
@@ -16,7 +17,9 @@ public class AlbumValidator implements Validator{
 
 	@Override
 	public void validate(Object target, Errors errors) {
-		// TODO Auto-generated method stub
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "description", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "imagePath", "required");
 		
 	}
 
