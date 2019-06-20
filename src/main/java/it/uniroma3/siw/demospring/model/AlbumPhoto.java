e39af0378e9f9ac22ec382d47fcd05f92761edac
g185 <giuliano.martinelli97@gmail.com>
2019-06-20 13:51:04 +0200

    asss

---
+++ src/main/java/it/uniroma3/siw/demospring/model/AlbumPhoto.java
@@ -0,0 +1,19 @@
+package it.uniroma3.siw.demospring.model;
+
+public class AlbumPhoto {
+	
+	private String idAlbum;
+	private String idPhoto;
+	public String getIdAlbum() {
+		return idAlbum;
+	}
+	public void setIdAlbum(String idAlbum) {
+		this.idAlbum = idAlbum;
+	}
+	public String getIdPhoto() {
+		return idPhoto;
+	}
+	public void setIdPhoto(String idPhoto) {
+		this.idPhoto = idPhoto;
+	}
+}
